70dfc5ef87d07c68c7ac2329bcd07ec3229886c0
ABenn17282002 <a.benn17282002@gmail.com>
2024-09-13 08:15:58 +0000

    Link_DashBoard&Profile

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 export default function page() {
   return (
     <div>
-      <Button>Hellow World</Button>
+      {/* <Button>Hellow World</Button> */}
+      <Link href={{ pathname: '/dashboard' }}>/dashboard</Link>
+      <Link href={{ pathname: '/profile' }}>/profile</Link>
     </div>
   )
 }
